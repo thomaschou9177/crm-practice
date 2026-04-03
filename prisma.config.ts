6079aac6b299dd382d270dfdfd94e8d9933767da
Thomas <thomaschou9177@gmail.com>
2026-04-03 21:52:26 +0800

    delete dependencies @prisma/config and dotenv

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@prisma/config';
-import * as dotenv from 'dotenv';
-// Manually load .env file
-dotenv.config();
-export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-});
