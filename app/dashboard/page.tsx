e94da72472ad11e6049122b58f9cac13ac419666
Thomas <thomaschou9177@gmail.com>
2026-04-04 22:51:06 +0800

    remove onclick and use onsubmit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -366,17 +366,21 @@ export default async function DashboardPage(props:{
   </a>
   {/* ✅ 新增的跳頁 */}
     <JumpToPage totalPages={totalPages} currentPage={currentPage} />
-    <form action="/api/deleteAll" method="post">
+    <form
+      action="/api/deleteAll"
+      method="post"
+      onSubmit={() =>
+        confirm("⚠️ 確定要刪除所有資料嗎？此操作不可回復！")
+      }
+    >
       <button
         type="submit"
         className="bg-red-600 text-white px-6 py-2 rounded font-bold"
-        onClick={() =>
-          confirm("⚠️ 確定要刪除所有資料嗎？此操作不可回復！")
-        }
       >
         Delete All Data
       </button>
-  </form>
+    </form>
+
 </div>
 
 {/* 3. FILTERED RESULTS (CUSTOMER) */}
