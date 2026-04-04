233009005d147c0d4a27769e4a5c1ccf95a52874
Thomas <thomaschou9177@gmail.com>
2026-04-04 22:41:28 +0800

    try to fix api route error 2

--- app/api/deleteAll/route.ts
+++ app/api/deleteAll/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/db";
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
-
+export const runtime = "nodejs";  // ✅ 強制使用 Node.js runtime
 export async function POST(req:Request) {
   try {
     // 刪除 所有 customer data後，會自動刪除所有 customer_info data
