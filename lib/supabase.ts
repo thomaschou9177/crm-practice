3d32b2ef9a6fc36886bc41b6473b64657cdadd45
Thomas <thomaschou9177@gmail.com>
2026-04-08 02:47:23 +0800

    test 8

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
 // lib/supabase.ts
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // 建議用 Service Role Key，因為需要寫入 Storage
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
