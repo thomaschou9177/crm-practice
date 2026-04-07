d4b028c4735f73c2398a3cff7c212f56b22cd119
Thomas <thomaschou9177@gmail.com>
2026-04-08 03:42:09 +0800

    test 13

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY! // 後端用 service role key
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
