6b66df771455a202a5efa087895c3b73bc3a4bbf
Thomas <thomaschou9177@gmail.com>
2026-04-16 08:45:20 +0800

    fix by async and destruct promise object

--- app/[tenant]/page.tsx
+++ app/[tenant]/page.tsx
@@ -1,8 +1,14 @@
 // app/[tenant]/page.tsx (Server Component)
 import TenantLoginForm from "./TenantLoginForm";
 
-export default function TenantPage({ params }: { params: { tenant: string } }) {
-  const tenant = params.tenant; // 這裡一定有值
+// 這裡改成 async 函式
+export default async function TenantPage({ 
+  params 
+}: { 
+  params: Promise<{ tenant: string }> // 定義為 Promise
+}) {
+  // 使用 await 取得 tenant
+  const { tenant } = await params; 
   
   return <TenantLoginForm tenant={tenant} />;
 }
