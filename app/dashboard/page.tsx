41b55391a71180b44c06a717e820cbf35a5de0fe
Thomas <thomaschou9177@gmail.com>
2026-04-27 23:46:34 +0800

    try to fix 4

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 // app/dashboard/page.tsx
 "use client"
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const revalidate = false;
 import DeleteAllDataButton from '@/components/DeleteAllDataButton';
 import { DeleteColumnButton } from '@/components/DeleteColumnButton';
 import { DeleteRowButton } from '@/components/DeleteRowButton';
