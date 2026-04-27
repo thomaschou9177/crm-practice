ff047e0458458ccc0227c60b197f737bfe17173d
Thomas <thomaschou9177@gmail.com>
2026-04-28 00:25:34 +0800

    try to fix 5

--- lib/queue.ts
+++
@@ -1,6 +0,0 @@
-import { Queue } from "bullmq";
-import Redis from "ioredis";
-
-const connection = new Redis(process.env.REDIS_URL!);
-
-export const excelQueue = new Queue("excelQueue", { connection });
