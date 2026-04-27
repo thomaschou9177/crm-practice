5daba2fddccfda488798071be16a43d592ff4a32
Thomas <thomaschou9177@gmail.com>
2026-04-28 00:32:04 +0800

    try to fix 6

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 // lib/db.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { [key: string]: PrismaClient };
 
