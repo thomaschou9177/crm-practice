6b98caef8851f5de69da73f03ee3f928f5750b42
Thomas <thomaschou9177@gmail.com>
2026-05-06 17:19:02 +0800

    use sessionStorage 2

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,13 +1,22 @@
 // app/api/logout/route.ts
 import { destroySession } from '@/lib/session';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-  const { sessionId } = await request.json(); // 改從 Body 接收
+  try{
+    const { sessionId } = await request.json(); // 改從 Body 接收
 
-  if (sessionId) {
-    // ✅ 刪除資料庫中的紀錄
-    await destroySession(sessionId);
+    if (sessionId) {
+      // ✅ 刪除資料庫中的紀錄
+      await destroySession(sessionId);
+    }
+    // 2. 同時清除 Cookie
+    const cookieStore = await cookies();
+    cookieStore.delete('sessionId');
+    return new Response(null, { status: 204 });
+  }catch(error){
+    // 防止 Body 解析失敗導致崩潰
+    return new NextResponse(JSON.stringify({ error: 'Invalid Request' }), { status: 400 });
   }
-
-  return new Response(null, { status: 204 });
 }
