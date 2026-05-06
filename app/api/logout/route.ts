0135dc6cfdb1b01bd92c2e0861351b5a7f40dac6
Thomas <thomaschou9177@gmail.com>
2026-05-06 17:14:17 +0800

    use sessionStorage

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,15 +1,12 @@
 // app/api/logout/route.ts
 import { destroySession } from '@/lib/session';
-import { cookies } from 'next/headers';
 
 export async function POST(request: Request) {
-  const cookieStore = await cookies();
-  const sessionId = cookieStore.get('sessionId')?.value;
+  const { sessionId } = await request.json(); // 改從 Body 接收
 
   if (sessionId) {
     // ✅ 刪除資料庫中的紀錄
     await destroySession(sessionId);
-    cookieStore.delete('sessionId');
   }
 
   return new Response(null, { status: 204 });
